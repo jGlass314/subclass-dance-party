3ccc6b7beb7374743694e948a44eaf9d600d8be6
jglass314 <jGlass314@gmail.com>
2017-12-04 22:00:53 -0800

    got some animation action and a gif to load

--- src/discoDancer.js
+++ src/discoDancer.js
@@ -1,9 +1,8 @@
 var DiscoDancer = class extends Dancer {
   constructor(top, left, timeBetweenSteps) {
     super(top, left, timeBetweenSteps);
-    var gifString = '<img src="https://tenor.com/view/disco-simpsons-gif-5425409"></img>';
- //gifString = '';
- //   this.$node = $('<span class="dancer">' + gifString + '</span>');
-    console.log(this.$node);
+    var gifString = '<img src="https://media.giphy.com/media/3orif8Hkj7sCODCZaw/giphy.gif" width=10% height=10%></img>';
+    this.$node = $('<span class="hide">' + gifString + '</span>');
+//    console.log(this.$node);
   }
 };
--- src/init.js
+++ src/init.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   window.dancers = [];
 
+  var eachDancer = document.getElementsByClassName('dancer');
+
   $('.addDancerButton').on('click', function(event) {
     /* This function sets up the click handlers for the create-dancer
      * buttons on dancefloor.html. You should only need to make one small change to it.
@@ -19,25 +21,42 @@ $(document).ready(function() {
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
+    
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $('body').height() * Math.random() + 50,
-      $('body').width() * Math.random(),
+      ($('body').height() * Math.random() + 50) % ($('body').height() - 20),
+      ($('body').width() * Math.random()) % ($('body').width() - 20),
       /*Math.random() * */500
     );
     window.dancers.push(dancer);
-    console.log(dancer.$node);
+    
+//    console.log(dancer);
     $('body').append(dancer.$node);
+    var moveleft = true;
+    $('.hide, .dancer').on("mouseover", function(event) {
+      console.log('this:',this);
+      
+      if (moveleft === true) {
+        $(this).animate({left: '250px'}, 'fast');
+        moveleft = false;
+      } else {
+        $(this).animate({left: '500px'}, 'fast');
+        moveleft = true;
+      }
+    });
   });
   $('.lineUpDancers').on('click', function(event) {
-    var topDrift = 50;
+    var topDrift = 500;
     var sideDrift = 0;
     window.dancers.forEach(function(dancer) {
       dancer.setPosition(topDrift, sideDrift);
-      topDrift += 30;
+      topDrift -= 30;
       sideDrift += 20;
     });
   });
+  var eachDancer = document.getElementsByClassName('dancer');
+  console.log(eachDancer);
+
+  
 });
